aa1cb4a56fc478df9c59997ee77cf060cfbf25a3
daersh <daersh@naver.com>
2024-01-16 13:48:22 +0900

    ㅓ

--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section02/encapsulation/problem3/resolved/Application.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section02/encapsulation/problem3/resolved/Application.java
@@ -4,11 +4,12 @@ public class Application {
 
     public static void main(String[] args) {
         /*수업목표. 접근제어자와 캡슐화에 대해 이해하고 직접 필드 접근을 막는 이유를 이해할 수 있다.*/
-        /*설명. 캡슐화를 통해 직접적으로 접근을 막음!*/
+        /*설명. 캡슐화를 통해 직접적으로 접근을 막고 메소드를 통해 간접적으로 접근!!*/
         Monster monster1 = new Monster();
-        monster1.name = "드라큘라";
-        monster1.hp = 200;
-
+//        monster1.name = "드라큘라";
+//        monster1.hp = 200;
+        monster1.setInfo1("드라큘라");
+        monster1.setInfo2(1000);
 
 
     }
