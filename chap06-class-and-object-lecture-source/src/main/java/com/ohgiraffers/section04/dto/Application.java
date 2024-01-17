3c4e78c09c901427939f0197b291aa338be2188c
daersh <daersh@naver.com>
2024-01-17 10:15:39 +0900

    memo, lesson setting

--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/dto/Application.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/dto/Application.java
@@ -1,8 +1,12 @@
 package com.ohgiraffers.section04.dto;
 
 public class Application {
-    /*수업목표 생성자를 이용한 객체 초기화, 설정자를 이용한 초기화의 장단점을 이해할 수 있다.*/
 
+    /* 수업목표.
+                             ** 목표 **
+        ----------------------------------------------------
+            * 생성자를 이용한 객체 초기화, 설정자를 이용한 초기화의 장단점을 이해할 수 있다.
+        ----------------------------------------------------*/
     public static void main(String[] args) {
         UserDTO user1 = new UserDTO();
         user1.forInformation();
