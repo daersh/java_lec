d52cbabb33db51e56e1de66cfe51a92986b3ec64
daersh <daersh@naver.com>
2024-01-17 15:40:25 +0900

    ㅁ

--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section08/uses/MemberRepository.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section08/uses/MemberRepository.java
@@ -2,12 +2,15 @@ package com.ohgiraffers.section08.uses;
 
 public class MemberRepository {
 
+    /*설명. static 변수인 멤버 객체 배열은 데이터베이스로 생각*/
     private final static Member[] members;
-    private static int count;
 
     static {
         members = new Member[10];
     }
+    private static int count;
+
+    /*설명. 데이터베이스 개념인 맴버 객체 배열에 넘어온 회원 가입용 회원 객체들을 한명 씩 추가*/
 
     public static boolean store(Member[] newMembers) {
 
@@ -17,7 +20,7 @@ public class MemberRepository {
 
         return true;
     }
-
+    /*설명. 데이터베이스 개념에서부터 조회하는 기능*/
     public static Member[] findAllMembers() {
 
         return members;
