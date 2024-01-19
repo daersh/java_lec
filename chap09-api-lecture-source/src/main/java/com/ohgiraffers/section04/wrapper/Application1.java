6deea26f0505563b0ffd3218f8766c09142d480f
daersh <daersh@naver.com>
2024-01-19 15:50:56 +0900

    타입간 타입변환

--- chap09-api-lecture-source/src/main/java/com/ohgiraffers/section04/wrapper/Application1.java
+++ chap09-api-lecture-source/src/main/java/com/ohgiraffers/section04/wrapper/Application1.java
@@ -19,5 +19,21 @@ public class Application1 {
         /*설명. 기본자료형과 래퍼 클래스는 자동으로 박싱, 언박싱 가능*/
         Integer autoBoxingInt = intValue;
         int autoUnboxingValuew = autoBoxingInt;
+        anythingMethod(10); // 설명. Object obg= (Object)(Integer)10;
+
+        /*설명. 래퍼 클래스 값 비교*/
+        //참고. 리터럴을 받기 때문에 주소도 동일한 것을 알 수 있다.
+        // 래퍼 클래스도 리터럴값으로 오토박싱하면 결국 동일한 객체 하나만 관리된다.
+        Integer integerTest= (Integer)30;
+        Integer integerTest2 = (Integer)30;
+        System.out.println("integerTest== integerTest2: " + (integerTest==integerTest2));
+        System.out.println("integerTest.equals(integerTest2)"+ (integerTest.equals(integerTest2)));
+        System.out.println("integerTest address: "+ System.identityHashCode(integerTest));  // 주소 동일!
+        System.out.println("integerTest2 address: "+ System.identityHashCode(integerTest2));
+
+    }
+    /*설명. 매개변수가 Object인 메소드(모든 자료형의 인자 받을 수 있는 메소드)*/
+    public static void anythingMethod(Object obj){      // 설명. 10 -> Integer(autoboxing) -> Object(다형성)
+        System.out.println("obj: "+obj); // 설명. Object의 toString()이 아닌 Integer의 toString()이 실행 (동적 바인딩 )
     }
 }
