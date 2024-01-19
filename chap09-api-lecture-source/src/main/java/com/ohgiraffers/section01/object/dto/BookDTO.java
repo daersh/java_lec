98415a0252285e65a47e2926ced7c6f1a2128e47
daersh <daersh@naver.com>
2024-01-19 10:25:46 +0900

    9장 API
    run, DTO 생성

---
+++ chap09-api-lecture-source/src/main/java/com/ohgiraffers/section01/object/dto/BookDTO.java
@@ -0,0 +1,50 @@
+package com.ohgiraffers.section01.object.dto;
+
+public class BookDTO {
+    private int number;
+    private String title;
+    private String author;
+    private int price;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "BookDTO{" +
+                "number=" + number +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ chap09-api-lecture-source/src/main/java/com/ohgiraffers/section01/object/run/Application1.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.section01.object.run;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+        /* 수업목표.
+                                 ** 목표 **
+            --------------------------------------------------------------------
+                * object 클래스의 toString()메서드 오버라이딩 목적을 이해하고 활용할 수있다.
+            --------------------------------------------------------------------*/
+    }
+}
