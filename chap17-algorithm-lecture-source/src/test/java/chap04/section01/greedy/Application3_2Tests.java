9e318a9e178ae08d291de5346d9e840dc2371718
daersh <daersh@naver.com>
2024-02-01 10:45:06 +0900

    greedy 회의실 배정

--- chap17-algorithm-lecture-source/src/test/java/chap04/section01/greedy/Application3_2Tests.java
+++ chap17-algorithm-lecture-source/src/test/java/chap04/section01/greedy/Application3_2Tests.java
@@ -1,5 +1,4 @@
 package chap04.section01.greedy;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -13,43 +12,43 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-public class Application3Tests {
-    private static String input1;
-    private static Integer output1;
+public class Application3_2Tests {
+    private static String input1, input2;
+    private static Integer output1, output2;
 
     @BeforeAll
     public static void set() {
 
-        /* 예시1 */
-        input1 = "11\n" +
+        input1 = "5\n" +
                 "1 4\n" +
+                "2 3\n" +
                 "3 5\n" +
-                "0 6\n" +
-                "5 7\n" +
-                "3 8\n" +
-                "5 9\n" +
-                "6 10\n" +
-                "8 11\n" +
-                "8 12\n" +
-                "2 13\n" +
-                "12 14";
-        output1 = 4;
+                "4 6\n" +
+                "5 7\n";
+        output1 = 3;
+
+        input2 = "3\n" +
+                "3 3\n" +
+                "1 3\n" +
+                "2 3\n";
+        output2 = 2;
+
 
     }
 
     public static Stream<Arguments> provideSource() {
         return Stream.of(
-                arguments(input1, output1)
-
+                arguments(input1, output1),
+                arguments(input2, output2)
         );
     }
 
-    @DisplayName("greedy4")
-    @Timeout(value = 2000, unit = TimeUnit.MILLISECONDS)
+    @DisplayName("greedy3")
+    @Timeout(value = 1000, unit = TimeUnit.MILLISECONDS)
     @ParameterizedTest
     @MethodSource("provideSource")
-    public void greedy4test(String input, Integer output) throws Exception {
-        Integer result = Application3_1.solution(input);
+    public void greedy2test(String input, Integer output) throws Exception {
+        Integer result = Application3_2.solution(input);
         Assertions.assertEquals(output, result);
     }
 }
