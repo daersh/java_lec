ac7fc078a10e3383d92bdbdd4776f1697a9a77ea
daersh <daersh@naver.com>
2024-02-01 10:34:34 +0900

    greedy 1931.회의실 배정

--- chap17-algorithm-lecture-source/src/main/java/chap04/section01/greedy/Application3_1.java
+++ chap17-algorithm-lecture-source/src/main/java/chap04/section01/greedy/Application3_1.java
@@ -20,13 +20,26 @@ public class Application3_1 {
             time[i][0] = Integer.parseInt(st.nextToken());
             time[i][1] = Integer.parseInt(st.nextToken());
         }
-
+        /*설명. 종료시간을 기준으로 오름차순을 한다. 만약 종료시간이 같으면 시작시간이 작은것을 앞으로 한다.*/
         Arrays.sort(time,(o1,o2)->{
             if(o1[1]==o2[1]){
+                /*설명. 시작 시간이 늦은 순서로 정렬 기준을 잡는다.*/
                 return o1[0]-o2[0];
             }
+            //설명.종료시간이 같지 않을 때 종료 시간이 이름 회의부터 정렬되도록 기준을 잡는다.
             return o1[1]- o2[1];
         });
+
+
+
+        int end =0;
+        for (int i = 0; i < N; i++) {
+            if(time[i][0]>=end){
+                max_count++;
+                end= time[i][1];
+            }
+        };
+
         return  max_count;
     }
 
